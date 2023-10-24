68d1da86c7793c1efa46f54f386e51a5fafe4678
Adamanta1 <elena.brashkina2020@gmail.com>
2023-10-25 02:39:01 +0400

    solve morse decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    const array = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+
+        let word = expr.slice(i, i + 10);
+
+        if (!word.includes('*')) {
+            let result = '';
+            for (let n = 0; n < word.length; n += 2) {
+                let part = word.slice(n, n + 2);
+                if (part === '10') {
+                    result = result + '.';
+                } else if (part === '11') {
+                    result = result + '-';
+                } else if (part === '**') {
+                    result = result + ' ';
+                }
+            }
+            array.push(result);
+        } else {
+            array.push(' ');
+        }
+    }
+
+    let phrase = '';
+
+    for (let k = 0; k < array.length; k += 1) {
+        if (array[k] === ' ') {
+            phrase = phrase + ' ';
+        } else {
+            phrase = phrase + MORSE_TABLE[array[k]];
+        }
+    }
+
+    return phrase;
 }
 
 module.exports = {
